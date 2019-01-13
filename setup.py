ccb2c953a840e3804ab8e8acdd0d9412eede74ae
jakubski <jakub.kucharski@onet.pl>
2019-01-13 22:36:46 +0100

    add setup.py file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='messenger-server',
+    version='',
+    packages=['messenger_server'],
+    url='',
+    license='MIT',
+    author='Jakub Kucharski, Adam Kuder',
+    author_email='',
+    description=''
+)
