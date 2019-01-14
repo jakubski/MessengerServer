30b942c0c1719a9511f697222b80362cb0d5d1d8
jakubski <jakub.kucharski@onet.pl>
2019-01-14 10:47:28 +0100

    modify signup_positive_response test case

--- tests/test_responses.py
+++ tests/test_responses.py
@@ -1,9 +1,5 @@
-import os
 from messenger_server.responses import Responses
 
 def test_sign_up_positive_response():
-    assert Responses.SignUpResponse.get_positive_response() == (0).to_bytes(1, "big") + (4).to_bytes(1, "big")
-
-def test_bait():
-    assert 4 == 5
+    assert Responses.SignUpResponse.get_positive_response() == (0).to_bytes(1, "big") + (0).to_bytes(1, "big")
 
