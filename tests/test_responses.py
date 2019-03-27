9d33089a1e7c9761ea6790f709c554357654ecbd
jakubski <jakub.kucharski@onet.plpwd>
2019-03-28 00:12:44 +0100

    add user_management tests

--- tests/test_responses.py
+++
@@ -1,5 +0,0 @@
-from messenger_server.responses import Responses
-
-def test_sign_up_positive_response():
-    assert Responses.SignUpResponse.get_positive_response() == (0).to_bytes(1, "big") + (0).to_bytes(1, "big")
-
